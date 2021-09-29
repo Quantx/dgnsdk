83134b371c1d0c5e7a5ad8efe9412ffd748a1c31
Samuel Deutsch <sam.e.deutsch@gmail.com>
2021-09-29 01:15:09 -0700

    Operands

--- dgnmcc/mcc2/mcc2.c
+++ dgnmcc/mcc2/mcc2.c
@@ -4,6 +4,12 @@ unsigned int16_t curfno;
 
 #include "../../lib/utils.c"
 
+unsigned int8_t zerosize; // Current size of the zero page resident portion of the local stack
+unsigned int16_t locsize; // Current size of the local stack
+
+unsigned int32_t stmtid;
+
+
 #include "node.c"
 #include "expression.c"
 #include "statement.c"
